ed1a3d9508728ac3f566fa3ce82680d68f9b976f
pothyshwer <blackdragon0465@gmail.com>
2026-01-16 10:21:25 +0530

    Fix variable name from 'principle' to 'principal'

--- si.py
+++ si.py
@@ -1,28 +1,31 @@
 import sys
-if (sys.argv) == 4:
+
+if len(sys.argv) == 4:
     print("User input given")
+
     script_name = sys.argv[0]
-    principle= float(sys.argv[1])
+    principal = float(sys.argv[1])
     rate = float(sys.argv[2])
     time = float(sys.argv[3])
-    simple_interest=(principle * rate * time)/100
 
- print("principle:",principle )
-    print("rate:",rate )
-    print("time:",time)
-    print("simple interest:",simple_interest)
+    simple_interest = (principal * rate * time) / 100
+
+    print("Principal:", principal)
+    print("Rate:", rate)
+    print("Time:", time)
+    print("Simple Interest:", simple_interest)
 
 else:
     print("User input not given")
+
     script_name = sys.argv[0]
-    principle = 3.0
+    principal = 3.0
     rate = 2.0
     time = 40.0
-    simple_interest=(principle * rate * time)/100
 
-    print("principle:",principle )
-    print("rate:",rate )
-    print("time:",time)
-    print("simple interest:",simple_interest)
-    
-    
+    simple_interest = (principal * rate * time) / 100
+
+    print("Principal:", principal)
+    print("Rate:", rate)
+    print("Time:", time)
+    print("Simple Interest:", simple_interest)
